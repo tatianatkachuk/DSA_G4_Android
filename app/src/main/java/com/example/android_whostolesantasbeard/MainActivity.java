b417ec77d0f4d0c1ce5b05974292cf8632f79c29
FerlinPhasor <frederick.andrade@estudiantat.upc.edu>
2022-11-24 13:01:16 +0100

    Fresh Project Configuration, retrofit and gson dependencies included

---
+++ app/src/main/java/com/example/android_whostolesantasbeard/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.android_whostolesantasbeard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
