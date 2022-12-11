ed8d84f1d9952ebd606d5fd2a26c6a4d2c0331ef
omarferrer30 <omar.fallouk@estudiantat.upc.edu>
2022-12-11 20:16:55 +0100

    creada la tienda, da errores

---
+++ app/src/main/java/com/example/android_whostolesantasbeard/RetrofitStore.java
@@ -0,0 +1,50 @@
+package com.example.android_whostolesantasbeard;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.android_whostolesantasbeard.entities.Item;
+import com.example.android_whostolesantasbeard.entities.ShopItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class Store extends AppCompatActivity {
+
+    ArrayList<Item> items;
+    IWSSBService service= APIClient.getClient().create(IWSSBService.class);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrofitstore);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(view -> {
+            Call<List<Item>> call = service.itemsList(items);
+
+            call.enqueue(new Callback<List<Item>>() {
+                @Override
+                public void onResponse(Call<List<Item>> call, Response<List<Item>> response) {
+                    final TextView textView = (TextView) findViewById(R.id.textView);
+                    textView.setText(response.body().toString());
+                }
+                @Override
+                public void onFailure(Call<List<Item>> call, Throwable t) {
+                    final TextView textView = (TextView) findViewById(R.id.textView);
+                    textView.setText("Something went wrong: " + t.getMessage());
+                }
+            });
+        });
+    }
+}
+
+
